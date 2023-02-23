7d259e87f4906b2ca8de55e7cf1a9560c7a3ae9b
Rfemerson <emersonferreirar8@gmail.com>
2023-02-23 20:51:01 -0300

    Starting Develop 4

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("  1. Start Breathing Activity");
+        Console.WriteLine("  2. Start Reflection Activity");
+        Console.WriteLine("  3. Start Listing Activity");
+        Console.WriteLine("  4. Quit");
+        Console.Write("Select a choice from the menu: ");
+        string activityChoice = Console.ReadLine();
+
+        int numberChoice = int.Parse(activityChoice);
+
+        if (numberChoice == 1){ // Breathing Activity
+
+        }
+
+        else if (numberChoice == 2) { //Reflection Activity
+
+        }
+
+        else if (numberChoice == 3) { // Listing Activity
+
+        }
+    
+        else if (numberChoice == 4) { // Quit 
+            
+        }
+        
+        else { // Please start again
+
+        }
     }
 }
